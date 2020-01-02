74e159396100e94c8b665839b92b0be4f53cb7de
phxghhello <55549329+phxghhello@users.noreply.github.com>
2020-01-02 15:46:32 +0800

    登录页结构的初步搭建

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="app-container">
     <router-view></router-view>
   </div>
 </template>
@@ -10,6 +10,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.app-container{
+  height: 100%;
+}
 </style>
