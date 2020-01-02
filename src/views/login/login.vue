221f37977dfa192a16057b222badbf4471ad4450
phxghhello <55549329+phxghhello@users.noreply.github.com>
2020-01-02 15:05:35 +0800

    element-ui输入框

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,13 +1,205 @@
 <template>
-   <div>登录页面</div>
+  <div class="login-container">
+    <!-- 左侧表单 -->
+    <div class="left">
+      <div class="title-box">
+        <img src="@/assets/login_icon.png" alt />
+        <span class="title">黑马面面</span>
+        <span class="line"></span>
+        <span class="sub-title">用户登录</span>
+      </div>
+      <!-- 表单 -->
+      <el-form :model="logForm" ref="logForm" :rules="rules" class="login-form">
+        <el-form-item prop="phone">
+          <el-input v-model="logForm.phone" placeholder="请输入手机号" prefix-icon="el-icon-user"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="logForm.passsword" placeholder="请输入密码" prefix-icon="el-icon-lock" type="password"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-row>
+            <el-col :span="17">
+              <el-input v-model="logForm.code" placeholder="请输入验证码" prefix-icon="el-icon-key"></el-input>
+            </el-col>
+            <el-col :span="7">
+              <img class="captcha" src alt />
+            </el-col>
+          </el-row>
+        </el-form-item>
+        <el-form-item class="rule-box" prop="checked">
+          <el-checkbox v-model="logForm.checked">
+            我已阅读并同意
+            <el-link type="primary">用户协议</el-link>
+            <span>和</span>
+            <el-link type="primary">隐私条款</el-link>
+          </el-checkbox>
+        </el-form-item>
+        <el-form-item class="btn-box">
+          <el-button type="primary">登录</el-button>
+          <el-button>注册</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <!-- 右侧图片 -->
+    <div class="right">
+      <img src="@/assets/login_bg.png" alt="" />
+    </div>
+  </div>
 </template>
 
 <script>
+//验证逻辑的导入
+import  { checkMobile, checkAgree, checkEmail }  from '../../utils/validator.js'
+
 export default {
-  name: "login"
+  name: "login",
+  data(){
+    return {
+      logForm:{
+        phone:"188888888888",
+        passsword:'88888888',
+        code:'1234',
+        checked: false,
+      },
+      rules:{
+        phone:[
+          {required:true,message:"手机号不能为空"},
+          { validator: checkMobile }
+        ],
+        email:[
+          { required: true, message: "邮箱号不能为空" },
+          { validator: checkEmail }
+        ],
+        password: [
+          { required: true, message: "密码不能为空" },
+          { min: 6, max: 12, message: "密码长度为6~12个字符" }
+        ],
+        code: [
+          { required: true, message: "验证码不能为空" },
+          { min: 4, max: 4, message: "验证码长度为4" }
+        ],
+        checked: [{ validator: checkAgree }]
+      },
+
+    }
+  },
+  methods:{
+
+  },
 };
 </script>
 
 <style lang="less" scoped>
+.login-container {
+  height: 100%;
+  background: linear-gradient(
+    225deg,
+    rgba(1, 198, 250, 1),
+    rgba(20, 147, 250, 1)
+  );
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  .left {
+    width: 478px;
+    height: 550px;
+    background: rgba(245, 245, 245, 1);
+    padding: 48px;
+    .title-box {
+      display: flex;
+      align-items: center;
 
+      img {
+        width: 22px;
+        height: 17px;
+        margin-right: 16px;
+      }
+      .title {
+        font-size: 24px;
+        margin-right: 14px;
+      }
+      .line {
+        width: 1px;
+        background-color: #c7c7c7;
+        height: 27px;
+        margin-right: 12px;
+      }
+      .sub-title {
+        font-size: 22px;
+      }
+    }
+    .login-form {
+      margin-top: 29px;
+      .captcha {
+        display: block;
+        width: 110px;
+        height: 42px;
+      }
+    }
+
+    .rule-box {
+      .el-checkbox {
+        display: flex;
+        align-items: center;
+        .el-checkbox__label {
+          display: flex;
+          align-items: center;
+        }
+      }
+    }
+    .btn-box {
+      .el-button {
+        width: 100%;
+        margin-left: 0;
+        margin-bottom: 28px;
+      }
+    }
+  }
+  .register-dialog {
+    .el-dialog {
+      width: 600px;
+    }
+    .el-dialog__header {
+      background: linear-gradient(
+        right,
+        rgba(1, 198, 250, 1),
+        rgba(20, 147, 250, 1)
+      );
+      text-align: center;
+      .el-dialog__title {
+        color: white;
+      }
+    }
+    .captcha {
+      width: 100%;
+    }
+    .avatar-uploader {
+      display: flex;
+      justify-content: center;
+    }
+    .avatar-uploader .el-upload {
+      border: 1px dashed #d9d9d9;
+      border-radius: 6px;
+      cursor: pointer;
+      position: relative;
+      overflow: hidden;
+    }
+    .avatar-uploader .el-upload:hover {
+      border-color: #409eff;
+    }
+    .avatar-uploader-icon {
+      font-size: 28px;
+      color: #8c939d;
+      width: 178px;
+      height: 178px;
+      line-height: 178px;
+      text-align: center;
+    }
+    .avatar {
+      width: 178px;
+      height: 178px;
+      display: block;
+    }
+  }
+}
 </style>
