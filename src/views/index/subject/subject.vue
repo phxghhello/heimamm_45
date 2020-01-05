23443e94867508e2a1f69c8a6e3d32996d36e79f
phxghhello <55549329+phxghhello@users.noreply.github.com>
2020-01-06 00:38:53 +0800

    首页学科的头部

--- src/views/index/subject/subject.vue
+++ src/views/index/subject/subject.vue
@@ -1,29 +1,36 @@
 <template>
   <div class="subject-container">
     <!-- 学科头部 -->
-    <el-card class="box-card">
-      <el-form :inline="true" :model="formInline" class="demo-form-inline">
-        <el-form-item label="学科编号">
-          <el-input v-model="formInline.user"></el-input>
+    <el-card class="card-header">
+      <el-form :inline="true" :model="subjectForm" ref="subjectForm" class="demo-form-inline">
+        <el-form-item label="学科编号" prop="rid">
+          <el-input v-model="subjectForm.rid"></el-input>
         </el-form-item>
-        <el-form-item label="学科名称">
-          <el-input v-model="formInline.user"></el-input>
+        <el-form-item label="学科名称" prop="name">
+          <el-input v-model="subjectForm.name"></el-input>
         </el-form-item>
-        <el-form-item label="创建者">
-          <el-input v-model="formInline.user"></el-input>
+        <el-form-item label="创建者" prop="username" class="short-input">
+          <el-input v-model="subjectForm.username"></el-input>
         </el-form-item>
-        <el-form-item label="状态">
-          <el-select v-model="formInline.region">
-            <el-option label="禁用" value="quit"></el-option>
-            <el-option label="启用" value="start"></el-option>
+        <el-form-item label="状态" prop="status">
+          <el-select v-model="subjectForm.status" placeholder="请选择状态">
+            <el-option label="禁用" value="0"></el-option>
+            <el-option label="启用" value="1"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="onSubmit">搜索</el-button>
-          <el-button >清除</el-button>
-          <el-button type="primary"> <span class="plus">+</span> 新增学科 </el-button>
+          <el-button>清除</el-button>
+          <el-button type="primary" icon="el-icon-plus">
+            新增学科
+          </el-button>
         </el-form-item>
       </el-form>
+    </el-card>
+
+    <!-- 底部 -->
+    <el-card class="card-main">
+
     </el-card>
   </div>
 </template>
@@ -32,9 +39,11 @@
 export default {
   data() {
     return {
-      formInline: {
-        user: "",
-        region: ""
+      subjectForm: {
+        rid: "",
+        name: "",
+        username: "",
+        status: "",
       }
     };
   },
@@ -47,9 +56,17 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.subject-container{
-    .el-button--primary .plus{
-        font-weight: 500;
+.subject-container {
+  .el-button--primary .plus {
+    font-weight: 500;
+  }
+  .short-input{
+    .el-form-item__content .el-input{
+        width: 100px;
     }
+  }
+  .card-main{
+      margin-top: 19px;
+  }
 }
 </style>
