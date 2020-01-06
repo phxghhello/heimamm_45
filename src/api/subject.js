e4bfb9e80c4bfaf3f0b690ca0831f4fd1fddb2a1
phxghhello <55549329+phxghhello@users.noreply.github.com>
2020-01-06 17:37:46 +0800

    增加学科列表的axios请求接口

--- src/api/subject.js
+++ src/api/subject.js
@@ -1,7 +1,10 @@
 // 导入 axios
 import axios from "axios";
+//导入token
+import {getToken} from '../utils/token.js'
 
-export function getSubjectList() {
+//学科列表
+export function getSubjectList(data) {
     // 调用接口
     return axios({
         url: process.env.VUE_APP_BASEURL + "/subject/list",
@@ -9,7 +12,68 @@ export function getSubjectList() {
         // 跨域 是否携带 cookie
         withCredentials: true,
         headers:{
-            token:window.localStorage.getItem("mmtoken")
-        }
+            token:getToken()
+        },
+        data,
+    })
+}
+
+// 学科状态设置
+export function setSubjectStatus(id) {
+    // 调用接口
+    return axios({
+        url: process.env.VUE_APP_BASEURL + "/subject/status",
+        method: "post",
+        // 跨域 是否携带 cookie
+        withCredentials: true,
+        headers:{
+            token:getToken()
+        },
+        id,
+    })
+}
+
+//学科删除
+export function removeSubject(id) {
+    // 调用接口
+    return axios({
+        url: process.env.VUE_APP_BASEURL + "/subject/remove",
+        method: "post",
+        // 跨域 是否携带 cookie
+        withCredentials: true,
+        headers:{
+            token:getToken()
+        },
+        id,
+    })
+}
+
+//编辑学科
+export function editSubject(data) {
+    // 调用接口
+    return axios({
+        url: process.env.VUE_APP_BASEURL + "/subject/edit",
+        method: "post",
+        // 跨域 是否携带 cookie
+        withCredentials: true,
+        headers:{
+            token:getToken()
+        },
+        data,
+    })
+}
+
+//新增学科
+export function addSubject(data) {
+    // 调用接口
+    return axios({
+        url: process.env.VUE_APP_BASEURL + "/subject/add",
+        method: "post",
+        // 跨域 是否携带 cookie
+        withCredentials: true,
+        headers:{
+            token:getToken()
+        },
+        data,
     })
 }
