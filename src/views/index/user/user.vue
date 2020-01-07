ce3885fbdec8e84e91d118e3c03e8748f201fb6b
phxghhello <55549329+phxghhello@users.noreply.github.com>
2020-01-07 17:20:24 +0800

    用户模块 - 页面布局

--- src/views/index/user/user.vue
+++ src/views/index/user/user.vue
@@ -1,42 +1,39 @@
 <template>
-  <div class="subject-container">
-    <!-- 学科头部 -->
+  <div class="user-container">
+    <!-- 用户头部 -->
     <el-card class="card-header">
-      <el-form :inline="true" :model="subjectForm" ref="subjectForm" class="demo-form-inline">
-        <el-form-item label="学科编号" prop="rid">
-          <el-input v-model="subjectForm.rid"></el-input>
+      <el-form :inline="true" :model="userForm" ref="userForm" class="demo-form-inline">
+        <el-form-item label="用户名称" prop="username">
+          <el-input v-model="userForm.username"></el-input>
         </el-form-item>
-        <el-form-item label="学科名称" prop="name">
-          <el-input v-model="subjectForm.name"></el-input>
+        <el-form-item label="用户邮箱" prop="email">
+          <el-input v-model="userForm.email"></el-input>
         </el-form-item>
-        <el-form-item label="创建者" prop="username" class="short-input">
-          <el-input v-model="subjectForm.username"></el-input>
-        </el-form-item>
-        <el-form-item label="状态" prop="status">
-          <el-select v-model="subjectForm.status" placeholder="请选择状态">
-            <el-option label="禁用" value="0"></el-option>
-            <el-option label="启用" value="1"></el-option>
+        <el-form-item label="角色" prop="role_id">
+          <el-select v-model="userForm.role_id" placeholder="请选择角色">
+            <el-option label="超级管理员" value="1"></el-option>
+            <el-option label="管理员" value="2"></el-option>
+            <el-option label="老师" value="3"></el-option>
+            <el-option label="学生" value="4"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">搜索</el-button>
+          <el-button type="primary">搜索</el-button>
           <el-button>清除</el-button>
-          <el-button type="primary" icon="el-icon-plus">新增学科</el-button>
+          <el-button type="primary" icon="el-icon-plus">新增用户</el-button>
         </el-form-item>
       </el-form>
     </el-card>
 
     <!-- 底部 -->
     <el-card class="card-main">
-      <el-table :data="subjectTable">
+      <el-table :data="userTable">
         <el-table-column type="index" label="序号"></el-table-column>
-        <el-table-column prop="rid" label="学科编号"></el-table-column>
-        <el-table-column prop="name" label="学科名称"></el-table-column>
-        <el-table-column prop="short_name" label="简称"></el-table-column>
-        <el-table-column prop="username" label="创建者"></el-table-column>
-        <el-table-column label="创建日期">
-          <template slot-scope="scope">{{ scope.row.create_time | formatTime }}</template>
-        </el-table-column>
+        <el-table-column prop="username" label="用户名"></el-table-column>
+        <el-table-column prop="phone" label="电话"></el-table-column>
+        <el-table-column prop="email" label="邮箱"></el-table-column>
+        <el-table-column prop="role_id" label="角色"></el-table-column>
+        <el-table-column prop="remark" label="备注"></el-table-column>
         <el-table-column label="状态">
           <template slot-scope="scope">
             <span v-if="scope.row.status === 1">启用</span>
@@ -45,16 +42,16 @@
         </el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-button size="mini" type="text" @click="enterEdit(scope.row)">编辑</el-button>
-            <el-button size="mini" type="text">启用</el-button>
-            <el-button size="mini" type="text">删除</el-button>
+            <el-button type="text" @click="enterEdit(scope.row)">编辑</el-button>
+            <el-button type="text">启用</el-button>
+            <el-button type="text">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
       <!-- 分页器 -->
       <el-pagination
-        :current-page="currentPage"
-        :page-sizes="[5, 10, 15, 20]"
+        :current-page="page"
+        :page-sizes="[5, 6, 7, 8]"
         :page-size="limit"
         layout="total, sizes, prev, pager, next, jumper"
         :total="total"
@@ -70,39 +67,34 @@
 export default {
   data() {
     return {
-      subjectForm: {
-        rid: "",
-        name: "",
+      userForm: {
         username: "",
-        status: ""
+        email: "",
+        role_id: "",
       },
-      subjectTable: [
+      //暂时的假数据
+      userTable: [
         {
-          rid: "subject1",
-          name: "前端与移动开发",
-          short_name: "前端",
-          username: "管理员",
+          username: "user1",
+          email: "js@qq.com",
+          role_id: "管理员",
           create_time: "2019-12-4",
           status: 0
         },
         {
-          rid: "subject2",
-          name: "Java",
-          short_name: "后端",
-          username: "管理员",
+          username: "user2",
+          email: "Java@qq.com",
+          role_id: "管理员",
           create_time: "2019-12-5",
           status: 1
         }
       ],
-      currentPage: 1,
+      page: 1,
       total: 100,
       limit: 5
     };
   },
   methods: {
-    onSubmit() {
-      window.console.log("submit!");
-    },
     handleSizeChange(val) {
       window.console.log(`每页 ${val} 条`);
     },
@@ -114,7 +106,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.subject-container {
+.user-container {
   .el-button--primary .plus {
     font-weight: 500;
   }
