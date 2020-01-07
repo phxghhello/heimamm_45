7184e9a2862c8e7fc2e4035d6cd7424ccd2c703d
phxghhello <55549329+phxghhello@users.noreply.github.com>
2020-01-07 17:28:31 +0800

    用户模块 - api抽取

--- src/api/user.js
+++ src/api/user.js
@@ -31,3 +31,74 @@ export function logout() {
 }
 
 //-----------------------以下是用户列表的接口----------------------------------------
+//获取用户列表
+export function getUserList(params) {
+    // 调用接口
+    return axios({
+        url: process.env.VUE_APP_BASEURL + "/user/list",
+        method: "get",
+        // 跨域 是否携带 cookie
+        withCredentials: true,
+        headers:{
+            token:getToken()
+        },
+        params,
+    })
+}
+//创建用户
+export function addUser(data) {
+    // 调用接口
+    return axios({
+        url: process.env.VUE_APP_BASEURL + "/user/add",
+        method: "post",
+        // 跨域 是否携带 cookie
+        withCredentials: true,
+        headers:{
+            token:getToken()
+        },
+        data,
+    })
+}
+//设置用户状态
+export function setUserStatus(data) {
+    // 调用接口
+    return axios({
+        url: process.env.VUE_APP_BASEURL + "/user/status",
+        method: "post",
+        // 跨域 是否携带 cookie
+        withCredentials: true,
+        headers:{
+            token:getToken()
+        },
+        data,
+    })
+}
+
+//删除用户
+export function removeUser(data) {
+    // 调用接口
+    return axios({
+        url: process.env.VUE_APP_BASEURL + "/user/remove",
+        method: "post",
+        // 跨域 是否携带 cookie
+        withCredentials: true,
+        headers:{
+            token:getToken()
+        },
+        data,
+    })
+}
+//编辑用户
+export function editUser(data) {
+    // 调用接口
+    return axios({
+        url: process.env.VUE_APP_BASEURL + "/user/edit",
+        method: "post",
+        // 跨域 是否携带 cookie
+        withCredentials: true,
+        headers:{
+            token:getToken()
+        },
+        data,
+    })
+}
